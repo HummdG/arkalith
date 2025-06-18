4e44cb452239e4312638a027f9c948a93f72768f
HummdG <hummd2001@gmail.com>
2025-06-18 07:48:26 +0100

    test2

--- arkalith-landing/next.config.ts
+++ arkalith-landing/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    appDir: true, // Ensure App Router is enabled
-  },
-  trailingSlash: false,
-  output: 'standalone', // Try this if the issue persists
+  /* config options here */
 };
 
 export default nextConfig;
